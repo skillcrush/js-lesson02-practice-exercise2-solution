d66b4b42c12a2f7f12d5442ca2088887d7820740
tauri_s <tauristclaire@gmail.com>
2026-09-23 13:22:17 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+var name = prompt("What's your name?");
+var birthYear = prompt("What year were you born? (ex. 1988)");
+console.log(`Your name is ${name} and you're ${2026 - birthYear} years old.`);
